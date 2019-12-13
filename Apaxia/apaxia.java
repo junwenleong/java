d523dc169c4123ec22d0a40ad4346ff95fe76532
junwenleong <58838483+junwenleong@users.noreply.github.com>
2019-12-13 13:34:23 +0800

    Update apaxia.java

--- Apaxia/apaxia.java
+++ Apaxia/apaxia.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Apaxia {
+public class apaxia {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		StringBuilder inpt = new StringBuilder(sc.nextLine());
