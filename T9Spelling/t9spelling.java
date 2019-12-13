edf01d29246aa5b48207f78ab261c9dcd0363c3f
junwenleong <58838483+junwenleong@users.noreply.github.com>
2019-12-13 13:34:02 +0800

    Update t9spelling.java

--- T9Spelling/t9spelling.java
+++ T9Spelling/t9spelling.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class T9spelling {
+public class t9spelling {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int nocases = sc.nextInt();
