08e7eaa8a63a97a82c8d51f2886d39906ddccfe6
junwenleong <58838483+junwenleong@users.noreply.github.com>
2019-12-13 13:35:01 +0800

    Update peasoup.java

--- PeaSoup/peasoup.java
+++ PeaSoup/peasoup.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class PeaSoup {
+public class peasoup {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(); // number of restaurants
